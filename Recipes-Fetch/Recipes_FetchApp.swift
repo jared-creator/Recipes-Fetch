e606bfc3fc0c3a8bda78010d865e6a2195ff5c26
jared <jaredmurray43@icloud.com>
2025-05-25 21:33:29 -0700

    Initial Commit

---
+++ Recipes-Fetch/Recipes_FetchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Recipes_FetchApp.swift
+//  Recipes-Fetch
+//
+//  Created by Jared Work on 5/25/25.
+//
+
+import SwiftUI
+
+@main
+struct Recipes_FetchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
